44896ae401095a58f4b2cff2d5b7d3f16e96df08
94sylim <94sylim@gmail.com>
2022-02-27 01:16:54 +0900

    구현 객체를 생성하고, 연결하는 별도의 설정 클래스

---
+++ core/src/main/java/hello/core/config/AppConfig.java
@@ -0,0 +1,20 @@
+package hello.core.config;
+
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { // 객체의 생성과 연결 담당
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+    }
+
+}
