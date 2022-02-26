23448bb28cd5645875153368b21259f887ccae43
94sylim <94sylim@gmail.com>
2022-02-27 01:18:40 +0900

    관심사 분리 후 생성자 주입

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,10 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    // Service가 회원을 찾기 위해 MemberRepository를 살펴보고 메소드를 불러옴
-    // 그런데 MemberRepository는 역할만 담당하고 구현할 기능은 없음
-    // 따라서 구현체인 MemoryMemberRepository() 객체가 타입을 MemberRepository 받아 생성됨
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 어떤 구현 객체가 주입될지는 AppConfig에서 정해짐
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
