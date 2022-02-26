566a8e6a731b4fd45502d9cac9f16ec82b36e174
94sylim <94sylim@gmail.com>
2022-02-27 01:19:18 +0900

    관심사 분리 후 생성자 주입 - 테스트(자바 메소드 버전)

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.config.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -8,8 +9,9 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
 
-        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
